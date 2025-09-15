2c9fb6d869f1eeafa0228253b97a0ee2c7e5575d
Adam Lawrence-Holzschuher <95254250+abplayzGH@users.noreply.github.com>
2025-09-14 21:44:44 -0500

    Created VisionTest, basic april tag processing and pose data. Updated README.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Road Runner Quickstart
+# FTC Decode; Team 13337 Robozark
 
+Forked from road-runner-quickstart
 Check out the [docs](https://rr.brott.dev/docs/v1-0/tuning/).
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+
+@TeleOp(name = "VisionTest")
+public class VisionTest extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+//                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCamera(BuiltinCameraDirection.BACK)
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        telemetry.addLine("Initialized. Waiting for start...");
+        telemetry.update();
+
+        // 2. Wait for start
+        waitForStart();
+
+        while (!isStopRequested() && opModeIsActive()) {
+            if (!tagProcessor.getDetections().isEmpty()) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                if (tag.ftcPose != null) {
+                    telemetry.addData("x", tag.ftcPose.x);
+                    telemetry.addData("y", tag.ftcPose.y);
+                    telemetry.addData("z", tag.ftcPose.z);
+                    telemetry.addData("roll", tag.ftcPose.roll);
+                    telemetry.addData("pitch", tag.ftcPose.pitch);
+                    telemetry.addData("yaw", tag.ftcPose.yaw);
+                } else {
+                    telemetry.addLine("Tag detected, but pose not available");
+                }
+            } else {
+                telemetry.addLine("No AprilTags detected");
+            }
+            telemetry.update();
+        }
+    }
+}
