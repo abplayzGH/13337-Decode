7b31d4a24ae84f623bf83c778844a0467c7644a7
Adam Lawrence-Holzschuher <95254250+abplayzGH@users.noreply.github.com>
2025-09-15 21:03:39 -0500

    Thingsfdf fdsfsda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.mechanisms.MotorTest;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -17,6 +18,8 @@ public class VisionTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException{
+        MotorTest motor = new MotorTest();
+        motor.init(hardwareMap);
 
         AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
                 .setDrawAxes(true)
